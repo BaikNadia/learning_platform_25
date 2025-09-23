2c0c095a38f5af1bfa5163c603e5ca9b554d84b0
BaikNadia <baikowa.nadia@gmail.com>
2025-09-23 14:58:05 +0300

    change config/urls

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from materials.views import CourseViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
@@ -11,4 +15,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api/lessons/', include('materials.urls')),
     path('api/users/', include('users.urls')),
+    # JWT endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),   # вход
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), # обновление токена
 ]
