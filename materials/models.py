c1a59986696004ed03fcaae6e3f96ff57593a5f6
BaikNadia <baikowa.nadia@gmail.com>
2025-09-29 18:57:07 +0300

    change materials/models

--- materials/models.py
+++ materials/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Course(models.Model):
+    objects = None
     title = models.CharField(max_length=200, verbose_name="Название")
     preview = models.ImageField(upload_to='courses/previews/', verbose_name="Превью", blank=True, null=True)
     description = models.TextField(verbose_name="Описание")
@@ -20,7 +21,23 @@ class Course(models.Model):
         verbose_name_plural = "Курсы"
 
 
+class Subscription(models.Model):
+    objects = None
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name="Пользователь")
+    course = models.ForeignKey('materials.Course', on_delete=models.CASCADE, verbose_name="Курс")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        verbose_name = "Подписка"
+        verbose_name_plural = "Подписки"
+        unique_together = ('user', 'course')
+
+    def __str__(self):
+        return f"{self.user.email} → {self.course.title}"
+
+
 class Lesson(models.Model):
+    objects = None
     title = models.CharField(max_length=200, verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
     preview = models.ImageField(upload_to='lessons/previews/', verbose_name="Превью", blank=True, null=True)
