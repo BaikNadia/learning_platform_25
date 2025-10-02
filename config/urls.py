8eb74b87f3f5a2c7da8590666e240d7e16451eb4
BaikNadia <baikowa.nadia@gmail.com>
2025-10-02 15:00:17 +0300

    change config

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
 from materials import views
 from materials.views import CourseViewSet
 from rest_framework_simplejwt.views import (
@@ -12,11 +18,30 @@ from rest_framework_simplejwt.views import (
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # API маршруты от роутера
     path('api/', include(router.urls)),
+
+    # API
+    path('api/', include('materials.urls')),           # уроки
+    path('api/courses/', include('materials.urls')),  # курсы + подписка
+    path('api/users/', include('users.urls')),
 
     # Уроки
     path('api/lessons/', include('materials.urls')),
@@ -27,4 +52,7 @@ urlpatterns = [
     # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
