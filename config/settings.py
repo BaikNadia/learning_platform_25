1b875ddf2bb1ef4aca9daf5c55e32959ae4329a2
BaikNadia <baikowa.nadia@gmail.com>
2025-10-24 14:22:05 +0300

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -171,7 +171,7 @@ STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379",
         "OPTIONS": {
             "client_class": "django_redis.client.DefaultClient",
         }
